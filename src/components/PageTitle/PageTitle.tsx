d9d529dea1f13e4cff02e3073c57e9946e4cb9ae
Gabriel Alcântara <galcantaradev@gmail.com>
2022-03-12 19:28:15 -0300

    fix component name

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const PageTitle = styled.h1`
   background-color: ${props => props.theme.backgroundColor};
   color: ${props => props.theme.textColor};
-  padding: 1rem;
   text-align: center;
   text-decoration: underline;
 `;
